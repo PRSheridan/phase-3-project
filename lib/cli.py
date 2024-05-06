044ca255de5dbfedb0556ea63c853330c23b42e2
Phil <philrsheridan@gmail.com>
2024-05-06 16:04:22 -0400

    Leaderboard, Statistics, Reset, Game Features

--- lib/cli.py
+++ lib/cli.py
@@ -4,14 +4,19 @@ from helpers import (
     exit_program,
     login,
     begin_test,
+    leaderboard,
     stats,
-    change_name
+    change_name,
+    reset_stats,
+    delete_user
 )
 
 def main():
     logged_in = False
     username = None
     while True:
+
+#LOGIN SCREEN -------------------------------------------------------------------------
         if (logged_in == False):
             menu(logged_in)
             choice = input("> ")
@@ -20,60 +25,74 @@ def main():
             elif choice == "1":
                 username = login()
                 logged_in = True
+
+#MAIN MENU -----------------------------------------------------------------------------
         else:
             menu(logged_in)
             choice = input("> ")
             if choice == "0":
                 exit_program()
             elif choice == "00":
-                username = None
                 logged_in = False
-            elif choice == "1":
-                begin_test()
-            #game will live here:
-            #take 3 random sentences
-            #countdown
-            #capture input
-            #capture time
-            #compare to sentence
-            #evaluate accuracy
-            #update test history
-            #update user information
+                username = None
 
+##BEGIN GAME -----------------------------------------------------------------------------
+            elif choice == "1":
+                begin_test(username)
+
+##LEADERBOARD ----------------------------------------------------------------------------
             elif choice == "2":
                 leaderboard()
+
+#STATISTICS MENU ------------------------------------------------------------------------
             elif choice == "3":
                 stats(username)
-                user_menu()
+                stats_menu()
                 choice = input("> ")
+
+##CHANGE USERNAME ------------------------------------------------------------------------
                 if(choice == "1"):
                     username = change_name(username)
+                    print(f"Username has been changed to: {username}")
+
+##RESET STATS ----------------------------------------------------------------------------
                 elif(choice == "2"):
-                    print("test")
+                    reset_stats(username)
+                    print("Statistics reset")
+
+##DELETE USER ----------------------------------------------------------------------------
+                elif(choice == "3"):
+                    delete_user(username)
+                    logged_in = False
+                    username = None
                 elif(choice == "0"):
-                    print("test")
+                    print("Navigating...")
                 else:
                     print("Invalid choice")
-
             else:
                 print("Invalid choice")
-
+#CLI MENU ----------------------------------------------------------------------------------
 def menu(logged_in):
-    print("Please select an option:")
     if (logged_in == False):
+        print("Please login to continue:")
         print("1. Login")
         print("0. Quit")
     else:
+        print("--------------------------------| Placeholder Name |--------------------------------")
+        print("Please select an option:")
         print("1. Begin test")
-        print("2. Scoreboard")
+        print("2. Leaderboard")
         print("3. Statistics")
         print("00. Logout")
         print("0. Quit")
+        print("-------------------------------------------------------------------------------------")
 
-def user_menu():
+#STATS MENU ----------------------------------------------------------------------------------
+def stats_menu():
     print("Please select an option:")
     print("1. Change username")
     print("2. Reset stats")
+    print("3. Delete User")
     print("0. Back")
 
 if __name__ == "__main__":
--- lib/helpers.py
+++ lib/helpers.py
@@ -3,6 +3,9 @@ from models.sentence import Sentence
 from models.test import Test
 from models.user import User
 
+import time
+import random
+
 def login():
     login = True
     username = input("Please enter a unique username: ")
@@ -16,26 +19,66 @@ def login():
             print("Error creating new profile:", exc)
     return username
 
-def begin_test():
-    print("Beginning test: ")
+def begin_test(username):
+    print("--------------------------------| INITIALIZING |--------------------------------")
+    input("Press enter to begin: ")
+    print("3...")
+    time.sleep(1)
+    print("2...")
+    time.sleep(1)
+    print("1...")
+    time.sleep(1)
+    print("GO")
+    test(username)
+
+def test(username):
+    user = User.find_by_name(username)
+    sentences = Sentence.get_all()
+    test_sentence = sentences[random.randint(0, (len(sentences)-1))]
+    print(test_sentence.string)
+    time.sleep(.2)
+    start = time.time()
+    test_answer = input()
+    end = time.time()
+    final_time = round((end - start), 1)
+    print(f"time: {final_time} seconds")
+    Test.create(test_answer, final_time, 4, user.id, test_sentence.id)
+
+
+def leaderboard():
+    print("--------------------------------| LEADERBOARD |--------------------------------")
+    leaderboard_data = []
+    users = User.get_all()
+    for user in users:
+        if all_tests := Test.find_by_user_id(user.id):
+            leaderboard_data.append(calculate_stats(user.name))
+        else:
+            leaderboard_data.append({
+                "username": user.name,
+                "total_tests": 0,
+                "avg_time": 999,
+                "avg_accuracy": 999,
+            })
+
+    sorted_data = sorted(leaderboard_data, key=lambda x: x['avg_time'])   
+    index = 1 
+    for user in sorted_data:
+        print(f"{index}. {user['username']}: Average Time: {round(user['avg_time'], 1)}")
+        index+=1
+    print("-------------------------------------------------------------------------------")
 
 
 def stats(username):
+    print(f"--------------------------------| {username} Statistics |--------------------------------")
     user = User.find_by_name(username)
     if all_tests := Test.find_by_user_id(user.id):
-        total_tests = len(all_tests)
-        total_time = 0
-        total_accuracy = 0
-        for test in all_tests:
-            total_time += test.time
-            total_accuracy += test.accuracy
-
-        print(f"STATS: {username}")
-        print(f"Tests taken: {total_tests}")
-        print(f"Average time: {total_time/total_tests}")
-        print(f"Average accuracy: {total_accuracy/total_tests}")
+        temp_stats = calculate_stats(username)
+        print(f"Tests taken: {temp_stats['total_tests']}")
+        print(f"Average time: {round(temp_stats['avg_time'], 1)}")
+        print(f"Average accuracy: {round(temp_stats['avg_accuracy'], 1)}")
     else:
-        print("User has no statistics")
+        print(f"No stats found.")
+    print("----------------------------------------------------------------------------------------")
 
 def change_name(username):
     user = User.find_by_name(username)
@@ -43,7 +86,44 @@ def change_name(username):
     user.update()
     return user.name
 
+def reset_stats(username):
+    user = User.find_by_name(username)
+    if all_tests := Test.find_by_user_id(user.id):
+        for test in all_tests:
+            test.delete()
+    
+
+def delete_user(username):
+    user = User.find_by_name(username)
+    confirm = input("Type Y/N to confirm deletion: ")
+    if confirm == "Y":
+        user.delete()
+        print("User Account Terminated")
+    else:
+        print("Action Cancelled")
 
 def exit_program():
     print("Shutting down testing chamber...")
     exit()
+
+#Helpers Helpers
+def calculate_stats(username):
+    stats = {
+        "username": username,
+        "total_tests": 0,
+        "avg_time": 0,
+        "avg_accuracy": 0,
+    }
+
+    user = User.find_by_name(username)
+    if all_tests := Test.find_by_user_id(user.id):
+        stats["total_tests"] = len(all_tests)
+        total_time = 0
+        total_accuracy = 0
+        for test in all_tests:
+            total_time += test.time
+            total_accuracy += test.accuracy
+
+        stats["avg_time"] = total_time / stats["total_tests"]
+        stats["avg_accuracy"] = total_accuracy / stats["total_tests"]
+        return stats
